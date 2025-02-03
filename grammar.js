dae566a5caf7b988183b1e19e0f2d732935eee85
Bullish Design <BullishDesignLLC@gmail.com>
2025-02-03 14:03:54 -0500

    Updating Tagged Treesitter Python Grammar

--- grammar.js
+++ grammar.js
@@ -66,7 +66,7 @@ module.exports = grammar({
   supertypes: $ => [
     $._simple_statement,
     //$.tagged_compound_statement,
-    //$._tagged_simple_statements,
+    //$._tagged_simple_statement,
     $._compound_statement,
     //$._tagged_compound_statement,
     $.expression,
@@ -121,7 +121,7 @@ module.exports = grammar({
   inline: $ => [
     $._simple_statement,
     //$.tagged_compound_statement,
-    $._tagged_simple_statements,
+    $._tagged_simple_statement,
     $._compound_statement,
     //$._tagged_compound_statement,
     $._suite,
